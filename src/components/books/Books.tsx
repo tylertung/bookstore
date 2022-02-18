72672503677b974535b3d1b587655f04c21442c9
tylertung <tung.do@dounets.com>
2022-02-18 11:05:13 +0700

    create  navbar

---
+++ src/components/books/Books.tsx
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Book from './Book'
+import Grid from '@mui/material/Grid'
+function Books() {
+
+  const [books,setBooks] = useState([{title: "", description: "",author_id: 0,author: {name: ""},genres: [{name: ""}] }])
+
+  useEffect(() => {    
+    const getBookAPI = async () => {
+      const bookAPI = await axios.get('http://localhost:3000/books')
+      setBooks(bookAPI.data)
+      
+    }
+    getBookAPI();
+  },[])
+  
+
+  return (
+    <Grid container 
+
+          alignItems="center"
+          justifyContent="left"
+          spacing={3}
+          sx={{width: 3/4,
+              margin: 'auto',
+              outline: 'solid'
+          }}
+    >
+        {books.map((book) => {
+          return (
+            <Grid item md={3} xs={6} sx={{padding: "1rem"}} key={book.title}>
+              <Book title={book.title}></Book>
+            </Grid>
+          )
+         
+        })
+        }
+
+    </Grid>
+    
+  )
+}
+
+export default Books
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import {makeStyles} from '@mui/styles'
+import {styled} from '@mui/material/styles'
+import React from 'react'
+
+const useStyles = makeStyles({
+  buttonStyle: {
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    '&:hover': {
+      backgroundColor: '#488462',
+    }
+  },
+});
+
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="sticky" >
+      <Toolbar>
+        <Typography 
+          variant="h5"
+          noWrap
+          component="div"
+          sx={{flexGrow: 1, margin: '1rem', fontWeight: "bold"}}
+        >
+          BookStore
+        </Typography>
+        <Button variant="outlined" sx={{marginRight: '1rem',color: "white", outline: "1px solid #84BC9C"}} className={classes.buttonStyle}>
+          Log in
+        </Button>
+        <Button variant="contained" color="info">
+          Sign up
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
