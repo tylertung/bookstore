e9ca7a9129fa68fb802c571d3db320652a270c1b
tylertung <tung.do@dounets.com>
2022-03-22 10:58:56 +0700

    update url for deploy

--- src/constant/urlRequest.ts
+++ src/constant/urlRequest.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://tyler-bookstore-api.herokuapp.com";
 
 export const loginUrl = "/login";
 export const authUrl = "/auth";
